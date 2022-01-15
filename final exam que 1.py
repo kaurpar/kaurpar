3470c7c0ebc7e92d3d384ac52af3c34812eae619
kaurpar <95943399+kaurpar@users.noreply.github.com>
2022-01-15 15:53:29 -0500

    Add files via upload
    submit final assignment

---
+++ final exam que 1.py
@@ -0,0 +1,28 @@
+secret_number =777
+
+
+print(
+"""
++=========================================+
+|Welcome to my game, muggle!              |                                                                  
+|Enter an integer number                  |          
+|and guess what number                    |
+|i have picked for u.                     |
+|So,what is the secret number ?           |                               
+|                                         |
++=========================================+
+""")
+n=int(input("Enter an integer number: "))
+if n != secret_number: # If statement to check the number is right
+    print("you are stuck in loop!")
+
+    if n <secret_number:
+        print("your imagination is too low.")
+    if n >secret_number:
+        print("your imagination is too up.")
+        
+    while True:
+
+            input("Enter an integer number: ") # Loop never ends...
+        
+print("Well done, muggle! You are free now,") # If the program made it here,that means the user's number correct
